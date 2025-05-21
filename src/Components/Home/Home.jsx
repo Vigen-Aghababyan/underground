912b7f157354fffec58aa7fd0de34c1a1bf8f522
lusine <lusine@u1s1.biz>
2025-05-21 16:51:29 +0400

    Add adjustments for mobile version

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import { useTranslation } from "react-i18next";
+import { useMediaQuery } from 'react-responsive';
 import classes from "./home.module.css"
 import NavBar from "./NavBar/NavBar";
 import image from "/images/Logo.png"
 const Home = () => {
-
+    const isMobile = useMediaQuery({ maxWidth: 767 });
     const { t } = useTranslation()
     
 
@@ -11,18 +12,20 @@ const Home = () => {
         <div className='wrapper'>
             <NavBar />
             <div className={classes.homeInfo}>
-                        <div className={classes.logo}>
-                            <img src={image} alt="logo" />
-                        </div>
-                        <div className={classes.text}>
-                    <span>{ t(`home.create`)}</span>
-                            <h1>{ t(`home.studio`)}</h1>
-                            <p>{ t(`home.cell`)}</p>
-                        </div>
-
+                <div className={classes.logo}>
+                    <img src={image} alt="logo"/>
                 </div>
+                <div className={classes.text}>
+                    <span>{t(isMobile ? `home.create_mobile` : `home.create`)}
+                </span>
+                <h1>{t(isMobile ? `home.studio_mobile` : `home.studio`)}</h1>
+                <p>{t(isMobile ? `home.cell_mobile` : `home.cell`)}</p>
             </div>
-    </div>);
+
+        </div>
+    </div>
+</div>)
+    ;
 }
- 
+
 export default Home
--- src/Components/Portfolio/Slider/Slider.jsx
+++ src/Components/Portfolio/Slider/Slider.jsx
@@ -1,50 +1,61 @@
 import { useState } from 'react';
-import classes from './slider.module.css'
-import left from '/images/Left.png'
-import right from '/images/Right.png'
+import classes from './slider.module.css';
+import left from '/images/Left.png';
+import right from '/images/Right.png';
 import { images } from '../../Constants/Constants';
-
+import { useMediaQuery } from 'react-responsive'; // (or implement your own mobile detection)
 
 const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const isMobile = useMediaQuery({ maxWidth: 767 }); // Detect mobile
 
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 3) % images.length);
-  };
+    // Show 1 image on mobile, 3 on desktop
+    const visibleCount = isMobile ? 1 : 3;
 
-  const goToPrevious = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 3 + images.length) % images.length
+    const goToNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + visibleCount) % images.length);
+    };
+
+    const goToPrevious = () => {
+        setCurrentIndex((prevIndex) =>
+            (prevIndex - visibleCount + images.length) % images.length
+        );
+    };
+
+    // Get visible images based on device
+    const visibleImages = [];
+    for (let i = 0; i < visibleCount; i++) {
+        visibleImages.push(images[(currentIndex + i) % images.length]);
+    }
+
+    return (
+        <div className={classes.sliderContainer}>
+            <button
+                className={`${classes.sliderButton} ${classes.left}`}
+                onClick={goToPrevious}
+            >
+                <img src={left} alt="PREV" />
+            </button>
+
+            <div className={classes.sliderImageContainer}>
+                {visibleImages.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`slider-image-${index}`}
+                        className={classes.sliderImage}
+                    />
+                ))}
+            </div>
+
+            <button
+                className={`${classes.sliderButton} ${classes.right}`}
+                onClick={goToNext}
+            >
+                <img src={right} alt="NEXT" />
+            </button>
+        </div>
     );
-  };
-
-  // Получаем 3 изображения, начиная с текущего индекса
-  const visibleImages = [
-    images[(currentIndex + 0) % images.length],
-    images[(currentIndex + 1) % images.length],
-    images[(currentIndex + 2) % images.length],
-  ];
-
-  return (
-      <div className={classes.sliderContainer}>
-      <button className={`${classes.sliderButton} ${classes.left}`} onClick={goToPrevious}>
-        <img src={left} alt="PREV" />
-      </button>
-          <div className={classes.sliderImageContainer}>
-        {visibleImages.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`slider-image-${index}`}
-            className={classes.sliderImage}
-          />
-        ))}
-      </div>
-          <button className={`${classes.sliderButton} ${classes.right}`} onClick={goToNext}>
-            <img src={right} alt="NEXT" />
-      </button>
-    </div>
-  );
 };
 
 export default Slider;
